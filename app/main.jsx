9e8b8eec103afc805a0b14f2d2ea1ff9ca07864a
fy62 <felicia.yau@gmail.com>
2017-01-11 17:35:59 -0500

    merged master

--- app/main.jsx
+++ app/main.jsx
@@ -38,10 +38,7 @@ render (
   <MuiThemeProvider>
     <Provider store={store}>
       <Router history={browserHistory}>
-        <Route path="/" component={ExampleApp}>
-          <IndexRedirect to="/jokes" />
-          <Route path="/jokes" component={Jokes} />
-        </Route>
+        <Route path="/" component={LandingPage} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
         <Route path="/products" component={Products} />
