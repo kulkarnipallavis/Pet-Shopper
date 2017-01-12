176efa8076c04d6729d633a27bf826a6047b9c0b
Garity Chapman <garitychapman@gmail.com>
2017-01-12 12:56:27 -0500

    Continuing work on order routes

--- server/order.js
+++ server/order.js
@@ -6,8 +6,7 @@ const Order = db.model('order')
 const router = require('express').Router()
 
 //get current active order
-router.get('/', (req, res, next) => {
-	//User Instance find active assoc. order
+app.param('activeOrder', function(req, res, next, id) {
 	Order.findOne({
 		where: {
 			id: req.user.id,
@@ -15,13 +14,45 @@ router.get('/', (req, res, next) => {
 		}
 	})
 	.then(order => {
-		
+		if (!order) throw error;
+		req.activeOrder = order;
+		next ();
+		return null;
 	})
-	//Order find assoc. products
-	// send all products
+	.catch(next);
 })
-//create new order
 
-//modify existing active order
+router.get('/', (req, res, next) => {
+	//Order find assoc. products
+	req.activeOrder.fetchProducts()
+	// send all products
+	.then(products => {
+		res.send(products)
+	})
+	.catch(next);
+})
+
+//create or modify an order
+router.post('/', (req, res, next) => {
+	if(req.activeOrder) {
+		Order.findById(req.activeOrder.id)
+		.then(order => order.Update({
+			products: req.body.products
+		})
+		.then(updatedOrder => {
+			return updatedOrder.fetchProducts()
+		})
+		.then(products => res.status(204).send(products))
+		.catch()
+	} else {
+		Order.Create(req.body)
+		.then(order => {
+			return order.fetchProducts()
+		})
+		.then(products => res.status(201).send(products))
+		.catch()
+	}
+	
+})
 
 //delete active order
