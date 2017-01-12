ce2e07cd8a6888c9fd501493c2594a867830fdd1
fy62 <felicia.yau@gmail.com>
2017-01-12 12:56:23 -0500

    styled product

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -34,37 +34,43 @@ export default connect (mapStateToProps, mapDispatchToProps) (
   render() {
     const styles = {
       root: {
+        height: '70%',
         display: 'flex',
-        flexWrap: 'wrap',
         justifyContent: 'space-around',
-      },
-      gridList: {
-        width: '80%',
-        height: '100%',
-        overflowY: 'auto',
-      },
+      }
     };
+
+// <Paper style={{height: 'auto', width: '40%', margin : '20px', backgroundImage: `url(${product.imageURL})`, backgroundSize: 'cover'}} zDepth={2} >
+//              <div style={{width: '100%', height: 'auto', display:'block', margin:'auto'}}>
+//              </div>
+//           </Paper>
+
+
+    //<img src={product.imageURL } style={{width: '100%', height: 'auto', display:'block', margin:'auto'}}/>
     const product = this.props.products.selectedProduct
     return (
       <div>
       <NavBar />
        <div style={styles.root} >
-          <Paper style={{height: 'auto', width: '40%', margin : '20px'}} zDepth={2} >
-             <img src={product.imageURL } style={{width: '100%', height: 'auto', display:'block', margin:'auto'}}/>
+          <Paper style={{maxHeight: '400px', width: '40%', margin : '20px'}} zDepth={2} >
+             <img src={product.imageURL } style={{maxWidth: '100%', height: '100%', display:'block', margin:'auto'}}/>
           </Paper>
 
           <Paper style={{height: 'auto', width: '40%', margin : '20px', display : 'block', position: 'relative'}} zDepth={1} >
-              <div style={{textAlign:'center'}}> 
-                {product.name}
+              <div style={{textAlign:'center'}}>
+                <h2>{product.name}</h2>
               </div>
-              <div style={{textAlign:'center'}}> 
+              <div style={{textAlign:'center', color:'green'}}>
+                <h4>In Stock.</h4>
+              </div>
+              <div style={{textAlign:'left'}}>
                 {product.description}
               </div>
-               <div style={{textAlign:'center'}}> 
-                {product.tags && product.tags.join(', ')}
+               <div style={{textAlign:'left', marginTop: '15px'}}>
+                {product.tags && ('Tags: ' + product.tags.join(', '))}
               </div>
-              <div style={{textAlign:'center'}}> 
-                ${product.price}
+              <div style={{textAlign:'center'}}>
+                <h4>Price: ${product.price}</h4>
               </div>
               <RaisedButton label="Add to Cart" labelColor='white' style={{ display : 'block', margin: 'auto'}}backgroundColor="#FA8072"/>
           </Paper>
