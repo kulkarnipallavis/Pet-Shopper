f29d644a73ccc73e8a35b4d6f6bc0ae83882e405
kulkarnipallavis <kulkarnipallavis@gmail.com>
2017-01-13 16:10:07 -0500

    fixed duplicate routes and removed jokes test file

--- app/main.jsx
+++ app/main.jsx
@@ -34,13 +34,9 @@ render (
   <MuiThemeProvider>
     <Provider store={store}>
       <Router history={browserHistory}>
-        <Route path="/" component={ExampleApp}>
-          <IndexRedirect to="/jokes" />
-          <Route path="/jokes" component={Jokes} />
-        </Route>
+        <Route path="/" component={LandingPage}>
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
-        <Route path="/" component={LandingPage} />
       </Router>
     </Provider>
   </MuiThemeProvider>,
