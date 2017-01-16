2055d23915ad9804c283f59ec979be0c121ca72d
fy62 <felicia.yau@gmail.com>
2017-01-16 12:48:47 -0500

    added navbar, changed label color, removed dummy info

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import Navbar from './Navbar';
 
 const style = {
   button : {
@@ -17,27 +18,30 @@ const style = {
 };
 
 export const Login = ({ login }) => (
-  <div style={ style.container }>
-      <h1 style={ style.title } >Sign Up</h1>
-        <form onSubmit={evt => {
-            evt.preventDefault()
-            login(evt.target.username.value, evt.target.password.value)
-          } }>
-            <TextField
-              name="username"
-              hintText="Email"
-              floatingLabelText="Email"
-            /><br />
-            <br />
-            <TextField
-              name="password"
-              hintText="Password"
-              floatingLabelText="Password"
-              type="password"
-            /><br />
-            <br />
-            <RaisedButton type="submit" value="Login" label="Login" backgroundColor="#FA8072" style={ style.button } />
-        </form>
+  <div>
+    <Navbar/>
+    <div style={ style.container }>
+        <h1 style={ style.title } >Login</h1>
+          <form onSubmit={evt => {
+              evt.preventDefault()
+              login(evt.target.username.value, evt.target.password.value)
+            } }>
+              <TextField
+                name="username"
+                hintText="Email"
+                floatingLabelText="Email"
+              /><br />
+              <br />
+              <TextField
+                name="password"
+                hintText="Password"
+                floatingLabelText="Password"
+                type="password"
+              /><br />
+              <br />
+              <RaisedButton type="submit" value="Login" label="Login" backgroundColor="#FA8072" style={ style.button } labelStyle={{color: 'white'}}/>
+          </form>
+    </div>
   </div>
 )
 
