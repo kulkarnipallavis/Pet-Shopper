48e13891918978de9bbbd283c31b0f99dcc904dc
Garity Chapman <garitychapman@gmail.com>
2017-01-16 11:35:16 -0500

    moved styles into central objects

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -6,6 +6,55 @@ import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import NavBar from './NavBar'
 
+const styles = {
+  root: {
+    height: '70%',
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  prodImage: {
+    maxWidth: '100%', 
+    height: '100%', 
+    display:'block', 
+    margin:'auto'
+  },
+  prodDescription: {
+    maxHeight: '400px', 
+    width: '40%', 
+    margin : '20px'
+  },
+  name:{
+    textAlign:'center'
+  },
+  stockStatus:{
+    textAlign:'center', 
+    color:'green'
+  },
+  description: {
+    textAlign:'left', 
+    paddingLeft: '10px', 
+    paddingRight: '10px'
+  },
+  tags:{
+    textAlign:'left', 
+    marginTop: '15px', 
+    paddingLeft: '10px', 
+    paddingRight: '10px'
+  },
+  price: {
+    textAlign:'center'
+  },
+  buttonContainer:{
+    display : 'block', 
+    bottom: '0', position: 
+    'absolute', 
+    right:'0'
+  },
+  button:{ 
+    display : 'block', 
+    margin: 'auto'
+  }
+}
 function mapStateToProps(state, ownProps) {
   return { products: state.products }
 }
@@ -32,56 +81,40 @@ export default connect (mapStateToProps, mapDispatchToProps) (
   }
 
   render() {
-    const styles = {
-      root: {
-        height: '70%',
-        display: 'flex',
-        justifyContent: 'space-around',
-      }
-    };
-
-// <Paper style={{height: 'auto', width: '40%', margin : '20px', backgroundImage: `url(${product.imageURL})`, backgroundSize: 'cover'}} zDepth={2} >
-//              <div style={{width: '100%', height: 'auto', display:'block', margin:'auto'}}>
-//              </div>
-//           </Paper>
-
-
-    //<img src={product.imageURL } style={{width: '100%', height: 'auto', display:'block', margin:'auto'}}/>
     const product = this.props.products.selectedProduct
     return (
       <div>
       <NavBar />
        <div style={styles.root} >
-          <Paper style={{maxHeight: '400px', width: '40%', margin : '20px'}} zDepth={2} >
-             <img src={product.imageURL } style={{maxWidth: '100%', height: '100%', display:'block', margin:'auto'}}/>
+          <Paper style={styles.prodImage} zDepth={2} >
+             <img src={product.imageURL } style={styles.prodDescription}/>
           </Paper>
 
-          <Paper style={{height: 'auto', width: '40%', margin : '20px', display : 'block', position: 'relative'}} zDepth={1} >
+          <Paper style={styles.prodDescription} zDepth={1} >
               <div style={{display : 'block'}}>
-                <div style={{textAlign:'center'}}>
+                <div style={styles.name}>
                   <h2>{product.name}</h2>
                 </div>
-                <div style={{textAlign:'center', color:'green'}}>
+                <div style={styles.stockStatus}>
                   <h4>In Stock.</h4>
                 </div>
-                <div style={{textAlign:'left', paddingLeft: '10px', paddingRight: '10px'}}>
+                <div style={styles.description}>
                   {product.description}
                 </div>
-                <div style={{textAlign:'left', marginTop: '15px', paddingLeft: '10px', paddingRight: '10px'}}>
+                <div style={styles.tags}>
                   {product.tags && ('Tags: ' + product.tags.join(', '))}
+                  }
                 </div>
-                <div style={{textAlign:'center'}}>
+                <div style={styles.price}>
                   <h4>Price: ${product.price}</h4>
                 </div>
               </div>
-              <div style={{display : 'block', bottom: '0', position: 'absolute', right:'0'}}>
-              <RaisedButton label="Add to Cart" labelColor='white' style={{ display : 'block', margin: 'auto'}}backgroundColor="#FA8072"/>
+              <div style={styles.buttonContainer}>
+              <RaisedButton label="Add to Cart" labelColor='white' style={styles.button}backgroundColor="#FA8072"/>
               </div>
           </Paper>
-
       </div>
-    </div>
-    )
+    </div>)
   }
 }
 )
--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router';
+
 import {getProduct, getProducts, setProduct} from '../reducers/products';
+import NavBar from './NavBar';
+
 import {GridList, GridTile} from 'material-ui/GridList';
 import Paper from 'material-ui/Paper';
-import {Link} from 'react-router'
-import NavBar from './NavBar'
 
 function mapStateToProps(state, ownProps) {
   return { products: state.products }
@@ -37,81 +39,9 @@ export class Products extends Component {
   }
 
   render() {
-    //if (!props.products.products.length) { return null }
-    const products = this.props.products.products;
-    const styles = {
-      root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-      },
-      gridList: {
-        width: '80%',
-        height: '100%',
-        overflowY: 'auto',
-      },
-    };
 
-    // const products = [
-    //   {
-    //     id: 1,
-    //     name: 'first',
-    //     imageURL: 'http://www.anniescostumes.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/f/i/file_name_32516.jpg',
-    //     price: 5.99,
-    //     description : 'first product description'
-    //   },
-    //   {
-    //     id: 2,
-    //     name: '2nd',
-    //     imageURL: 'http://images.asadart.com/sources/com/halloweenexpress/images/imagecache/354-375-ru887871.jpg',
-    //     price: 53.99,
-    //     description : 'second product description'
-    //   },
-    //   {
-    //     id: 3,
-    //     name: '3rd',
-    //     imageURL: 'https://img.costumecraze.com/images/vendors/california/PET20114-Nothin-But-A-Hound-Dog-Dog-Costume-large.jpg',
-    //     price: 15.99,
-    //     description : 'third product description'
-    //   },
-    //   {
-    //     id: 4,
-    //     name: 'fourth',
-    //     imageURL: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-9wAnahSjAL82jJl7uKPxA8TL4Zw_Q1Tb1d4ZsT4DWl-CnIQp',
-    //     price: 56.99,
-    //     description : 'fourth product description'
-    //   },
-    //   {
-    //     id: 5,
-    //     name: 'first',
-    //     imageURL: 'http://www.anniescostumes.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/f/i/file_name_32516.jpg',
-    //     price: 5.99,
-    //     description : 'fifth product description'
-    //   },
-    //   {
-    //     id: 6,
-    //     name: '2nd',
-    //     imageURL: 'http://images.asadart.com/sources/com/halloweenexpress/images/imagecache/354-375-ru887871.jpg',
-    //     price: 53.99,
-    //     description : 'sixth product description'
-    //   },
-    //   {
-    //     id: 7,
-    //     name: '3rd',
-    //     imageURL: 'https://img.costumecraze.com/images/vendors/california/PET20114-Nothin-But-A-Hound-Dog-Dog-Costume-large.jpg',
-    //     price: 15.99,
-    //     description : 'seventh product description'
-    //   },
-    //   {
-    //     id: 8,
-    //     name: 'fourth',
-    //     imageURL: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR-9wAnahSjAL82jJl7uKPxA8TL4Zw_Q1Tb1d4ZsT4DWl-CnIQp',
-    //     price: 56.99,
-    //     description : 'eighth product description'
-    //   }
-    // ]
-    // this.props.products.products = products;
-   
+    const products = this.props.products.products;
+    
    
     return (
       <div>
@@ -129,13 +59,13 @@ export class Products extends Component {
               onClick={()=>this.handleClick(index)}
             >
             <Link to={`/products/${product.id}`}>
-              <Paper style={{maxWidth: '250px', height: '200px', width: 'auto', margin : '20px'}} zDepth={2} >
+              <Paper style={styles.product} zDepth={2} >
                 <div>
-                  <img id='product-image' src={product.imageURL } style={{maxHeight: '150px', width: 'auto', display:'block', margin:'auto'}}/>
-                  <div id='product-name' style={{textAlign:'center'}}>
+                  <img id='product-image' src={product.imageURL } style={styles.prodImage}/>
+                  <div id='product-name' style={styles.centerElements}>
                     {product.name}
                   </div>
-                  <div style={{textAlign:'center'}}>
+                  <div style={styles.centerElements}>
                     ${product.price}
                   </div>
                 </div>
@@ -145,11 +75,36 @@ export class Products extends Component {
           ))}
         </GridList>
       </div>
-    </div>
-    )
-
-
+    </div>)
   }
 }
+
+const styles = {
+      root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      },
+      gridList: {
+        width: '80%',
+        height: '100%',
+        overflowY: 'auto',
+      },
+      product: {
+        maxWidth: '250px', 
+        height: '200px', 
+        width: 'auto', 
+        margin : '20px'
+      },
+      prodImage: {
+        maxHeight: '150px', 
+        width: 'auto', 
+        display:'block', 
+        margin:'auto'
+      },
+      centerElements: {
+        textAlign:'center'
+      },
+    };
 
 export default connect (mapStateToProps, mapDispatchToProps) (Products)
