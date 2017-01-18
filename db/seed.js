e96fe9c56c4c4c1f566effb71e2d90cc83096c73
annaebrown <annabrown4789@gmail.com>
2017-01-17 20:07:25 -0500

    accessories items for see file

--- db/seed.js
+++ db/seed.js
@@ -13,16 +13,14 @@ const seedCategories = () => db.Promise.map([
 
 const seedProducts = () => db.Promise.map([
 	{name: 'Bee Costume', category_id: 1, imageURL: '/images/clothing/beecostume.jpg', description: 'Bee costume for minature dogs. Made in America', price: 2899, tags:["costume", "clothing", "halloween"]},
+	{name: 'Red coat', category_id: 1, imageURL: '/images/clothing/redcoat.jpg', description: 'Red coat. Made in America', price: 2899, tags:["outerwear", "clothing"]},
+	{name: 'Red sweatshirt', category_id: 1, imageURL: '/images/clothing/redsweatshirt.jpg', description: 'Red sweatshirt. Made in America', price: 2899, tags:["casual", "clothing", "halloween"]},
 	{name: 'Blue T-Shirt', category_id: 1, imageURL: '/images/clothing/blueshirt.jpg', description: 'Plain blue shirt for minature dogs. Made in America', price: 1899, tags:["casual", "clothing"]},
 	{name: 'Tribal Patterned Dress', category_id: 1, imageURL: '/images/clothing/dress.jpg', description: 'Handmade dress for fancy occasions. Made in America', price: 4899, tags:["costume", "clothing", "dress"]},
-	{name: 'Gray Scarf', category_id: 1, imageURL: '/images/clothing/grayscarf.jpg', description: 'Gray woolen scarf. Made in America', price: 899, tags:["outerwear", "clothing", "scarf"]},
 	{name: 'Gray Sweatshirt', category_id: 1, imageURL: '/images/clothing/graysweatshirt.jpg', description: 'Gray sweatshirt for minature dachsunds. Made in America', price: 2899, tags:["outerwear", "clothing", "casual"]},
-	{name: 'Orange Coat', category_id: 1, imageURL: '/images/clothing/orangecoat.jpg', description: 'Fancy coat for minature dachsunds. Made in America', price: 3899, tags:["outerwear", "clothing"]},
 	{name: 'Patterned Coat', category_id: 1, imageURL: '/images/clothing/patternedcoat.jpg', description: 'Coat for minature dachsunds. Made in America', price: 2899, tags:["outerwear", "clothing"]},
 	{name: 'Pink shirt', category_id: 1, imageURL: '/images/clothing/pinkshirt.jpg', description: 'Pink shirt for minature dachsunds. Made in America', price: 1099, tags:["casual", "clothing"]},
 	{name: 'Pink sweater', category_id: 1, imageURL: '/images/clothing/pinksweater.jpg', description: 'Pink sweater for minature dachsunds. Made in America', price: 1899, tags:["sweater", "clothing"]},
-	{name: 'Red coat', category_id: 1, imageURL: '/images/clothing/redcoat.jpg', description: 'Red coat. Made in America', price: 2899, tags:["outerwear", "clothing"]},
-	{name: 'Red sweatshirt', category_id: 1, imageURL: '/images/clothing/redsweatshirt.jpg', description: 'Red sweatshirt. Made in America', price: 2899, tags:["casual", "clothing", "halloween"]},
 
 	{name: 'Stella and Chewys', category_id: 2, imageURL: '/images/food/stellaandchewys.jpg', description: 'Raw grass-fed beef. 100% complete & balanced dinner. Enchanced with probiotics & antioxidants', price: 1899, weight: "5.5oz", tags:["raw", "grain-free"]},
 	{name: 'Primal Raw', category_id: 2, imageURL: '/images/food/primalraw.jpeg', description: 'Raw grass-fed duck. Grain and gluten free. Made in the USA!', price: 2099, weight: "5.5oz", tags:["raw", "grain-free"]},
@@ -32,7 +30,14 @@ const seedProducts = () => db.Promise.map([
 	{name: 'Buffalo Blue', category_id: 2, imageURL: '/images/food/buffaloblue.jpeg', description: 'Features real meat and includes only the finest natural ingredients', price: 1099, tags:["dry", "grain-free"]},
 	{name: 'Weruva Grain-Free Selects', category_id: 2, imageURL: '/images/food/weruva.jpeg', description: 'Great for allergy-sensitive pooches. Whole chunks of wild caught tuna mixed with sweet potatoes.', price: 1099, tags:["wet", "grain-free"]},
 	{name: 'Halo Spot\'s Stew', category_id: 2, imageURL: '/images/food/halo.png', description: 'Wholesome, pure, and simple recipe for dogs. A complete and balanced formula.', price: 1099, tags:["wet", "grain-free"]},
-	{name: 'Nutro', category_id: 2, imageURL: '/images/food/nutro.jpeg', description: 'Provides a wholesome and delicious taste. Enhances skin and coat quality', price: 1099, tags:["wet", "grain-free"]}
+	{name: 'Nutro', category_id: 2, imageURL: '/images/food/nutro.jpeg', description: 'Provides a wholesome and delicious taste. Enhances skin and coat quality', price: 1099, tags:["wet", "grain-free"]},
+
+	{name: 'Gray scarf', category_id: 3, imageURL: '/images/accessories/grayscarf.jpg', description: 'Gray scarf. Made in America', price: 2899, tags:["outerwear", "clothing"]},
+	{name: 'Dog blanket', category_id: 3, imageURL: '/images/accessories/dogblanket.jpg', description: 'Blue wool dog blanket. Keeps your puppy warm on cold winter nights. Made in America', price: 2899, tags:["accessories", "blanket", "wool"]},
+	{name: 'Double leash', category_id: 3, imageURL: '/images/accessories/doubleleash.jpg', description: 'Double leather leash. Made in America', price: 1899, tags:["accessories", "leather"]},
+	{name: 'Santa beard', category_id: 3, imageURL: '/images/accessories/santabeard.jpg', description: 'Santa beard. Made in America', price: 1899, tags:["accessories", "costume"]},
+	{name: 'Snow hat', category_id: 3, imageURL: '/images/accessories/snowhat.jpg', description: 'Snow hat. Made in America', price: 2399, tags:["accessories", "costume"]},
+	{name: 'Dog tie', category_id: 3, imageURL: '/images/accessories/tie.jpg', description: 'Plaid tie. Made in America', price: 1899, tags:["accessories", "costume"]}
 
 	], product => db.model('products').create(product));
 
